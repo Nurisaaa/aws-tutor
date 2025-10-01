206ec4898b44e197cccf94f033bfd332468a0ccc
Nurisa <nurisa.abdykalykova03@gmail.com>
2025-10-01 17:38:41 +0600

    intial commit

---
+++ src/main/java/peaksoft/awstutorial/AwsTutorialApplication.java
@@ -0,0 +1,21 @@
+package peaksoft.awstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class AwsTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTutorialApplication.class, args);
+    }
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
